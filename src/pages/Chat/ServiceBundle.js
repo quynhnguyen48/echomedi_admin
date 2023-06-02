99bb60b41491f89767ff05dcaf34d9fef3f72c5d
test <test@gmail.com>
2023-06-02 21:35:25 +0700

    u

--- src/pages/Chat/ServiceBundle.js
+++ src/pages/Chat/ServiceBundle.js
@@ -182,14 +182,14 @@ const ServiceBundles = () => {
                     <button 
                     class="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none"
                     onClick={e => {
-                          navigate(`/chat/${c?.id}/${c.attributes.user?.data.attributes.email}`)
+                          navigate(`/chat/${c?.id}/${c.attributes.user?.data?.attributes.email}`)
                         }}>
                     <img class="object-cover w-10 h-10 rounded-full"
                       src="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg" alt="username" />
                     <div class="w-full pb-2">
                       <div class="flex justify-between">
                         
-                      <span class="block ml-2 font-semibold text-gray-600">{c.attributes.user.data.attributes?.patient.data.attributes.full_name}</span>
+                      <span class="block ml-2 font-semibold text-gray-600">{c.attributes.user.data.attributes?.patient.data?.attributes.full_name}</span>
                         {/* <span class="block ml-2 text-sm text-gray-600">25 minutes</span> */}
                       </div>
                       {/* <span class="block ml-2 text-sm text-gray-600">bye</span> */}
