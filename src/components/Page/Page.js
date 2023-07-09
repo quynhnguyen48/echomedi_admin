d015cd468d09b0bdde1290ae4e0a6a84f159f030
Quynh Nguyen <quynhnguyen@192.168.2.19>
2023-07-09 11:54:10 +0700

    u

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -12,7 +12,7 @@ const Page = ({
   return (
     <div className="flex flex-col h-screen">
       <Header title={title} parentUrl={parentUrl}/>
-      <div className={`sm:block flex sm:p-0 px-6 flex-1 overflow-y-auto ${className}`} id="pageContent">
+      <div className={`sm:block flex sm:p-0 px-6 flex-1 ${!isMobile && 'overflow-y-auto'} ${className}`} id="pageContent">
         <div className={`flex-1 sm:p-0 pb-6 pr-6 w-[100vw] ${contentClassName}`}>
           {children}
         </div>
