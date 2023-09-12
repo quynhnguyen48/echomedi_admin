3ccd24abde63e37f944b124e5f429ee578abef1d
Quynh Nguyen <quynhnguyen@Quynhs-MacBook-Air.local>
2023-09-12 14:20:56 +0700

    u

--- src/pages/Invoice/Components/InvoiceForm.js
+++ src/pages/Invoice/Components/InvoiceForm.js
@@ -364,10 +364,10 @@ const InvoiceForm = ({
         cliniqueServices: cliniqueServices?.filter(item => !item["attributes"]["paid"])
         .map((item) => ({
           id: item?.id,
-          price: item?.attributes?.price,
+          price: item?.attributes?.original_price ?? item?.attributes?.price,
           label: item?.attributes?.label,
-          discountFixedPrice: "",
-          discountPercentage: "",
+          discountFixedPrice: item?.attributes?.original_price * item.attributes["discount_percentage"] / 100,
+            discountPercentage: item.attributes["discount_percentage"],
           note: item?.attributes?.discount_note
         })),
         membership: membership ? [{
