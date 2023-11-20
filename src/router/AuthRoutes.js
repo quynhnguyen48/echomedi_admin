ef2efa9951e14d108568301f8bd472dcb7ab42db
Quynh Nguyen <you@example.com>
2023-11-20 12:51:54 +0700

    u

--- src/router/AuthRoutes.js
+++ src/router/AuthRoutes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 const Login = React.lazy(() => import("pages/Login"));
+const ForgotPassword = React.lazy(() => import("pages/ForgotPassword"));
 const HealthFinder = React.lazy(() => import("pages/HealthFinder"));
 const EnHealthFinder = React.lazy(() => import("pages/HealthFinderEN"));
 
@@ -10,6 +11,7 @@ const AuthRoutes = () => {
     <React.Suspense fallback={<span>Loading</span>}>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/login/:t" element={<Login />} />
         <Route path="/login/:t/:redirectUrl" element={<Login />} />
         <Route path="/health_finder" element={<HealthFinder />} />
