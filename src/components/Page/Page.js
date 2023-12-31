25392fca8f4475f63970acdcb5cfdd5986ab74f6
Your Name <you@example.com>
2023-12-31 17:54:07 +0700

    u

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -13,12 +13,12 @@ const Page = ({
   return (
     <div className="flex flex-col h-screen">
       <Header title={title} parentUrl={parentUrl}/>
-      <div className={`sm:block flex sm:p-0 flex-1 ml-2 sm:ml-0 ${!isMobile && 'overflow-y-auto'} ${className}`} id="pageContent">
+      <div className={`lg:block flex sm:p-0 flex-1 ml-2 sm:ml-0 ${!isMobile && 'overflow-y-auto'} ${className}`} id="pageContent">
         <div className={`flex-1 pr-1 pl-1 w-[100vw] ${contentClassName}`}>
           {children}
         </div>
         {rightContent && (
-          <div className={`px-2 ${isMobile ? 'w-full' : 'w-79'} ${rightContentClassName}`}>
+          <div className={`px-2 ${isMobile ? 'w-full' : 'w-[200px]'} ${rightContentClassName}`}>
             {rightContent}
           </div>
         )}
